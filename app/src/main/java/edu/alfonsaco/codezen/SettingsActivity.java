c1db4b5a348851333894a612f2df401daf634fae
alfonsaco <alfonso.rincondev@gmail.com>
2025-04-05 19:20:57 +0200

    Prueba de cambio de branch

--- app/src/main/java/edu/alfonsaco/codezen/SettingsActivity.java
+++ app/src/main/java/edu/alfonsaco/codezen/SettingsActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class SettingsActivity extends AppCompatActivity {
 
     private ImageView btnVolverDesdeAjustes;
+    //a
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
