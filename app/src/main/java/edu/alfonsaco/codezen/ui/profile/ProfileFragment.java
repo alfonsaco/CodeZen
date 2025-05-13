6454bf58dbb018fea23635da5eaf377b89e506d9
alfonsaco <alfonso.rincondev@gmail.com>
2025-05-13 18:46:35 +0200

    Se obtienen datos para el perfil

--- app/src/main/java/edu/alfonsaco/codezen/ui/profile/ProfileFragment.java
+++ app/src/main/java/edu/alfonsaco/codezen/ui/profile/ProfileFragment.java
@@ -1,32 +1,68 @@
 package edu.alfonsaco.codezen.ui.profile;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+import edu.alfonsaco.codezen.MainActivity;
+import edu.alfonsaco.codezen.R;
 import edu.alfonsaco.codezen.databinding.FragmentProfileBinding;
+import edu.alfonsaco.codezen.utils.BDD;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private BDD db;
+
+    // Componentes
+    private TextView txtNombreUsuario;
+    private ImageView avatarUsuario;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel homeViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
-
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textProfile;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        db=new BDD();
+
+        // Obtener datos del usuario
+        txtNombreUsuario=binding.txtNombreUsuario;
+        avatarUsuario=binding.avatarUsuario;
+
+        txtNombreUsuario.setText(MainActivity.username);
+        obtenerAvatar();
+
         return root;
+    }
+
+    // Obtener la imagen del usuario
+    private void obtenerAvatar() {
+        db.getDb()
+                .collection("usuarios")
+                .document(db.getUsuarioID())
+                .get()
+                .addOnSuccessListener(snapshot -> {
+                    String avatar=snapshot.getString("avatar");
+
+                    int idImagen=getResources().getIdentifier(avatar, "drawable", getActivity().getPackageName());
+                    avatarUsuario.setImageResource(idImagen);
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("ERROR", "No se pudo cargar el avatar");
+                });
     }
 
     @Override
